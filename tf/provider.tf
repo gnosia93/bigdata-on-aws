394a966e9a8591fa93107ce1c99c638e432105a2
Kwon <soonbeom@f8ffc2077dc2.ant.amazon.com>
2021-07-11 09:45:27 +0900

    ..

---
+++ tf/provider.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "ap-northeast-2"
+}
+
+terraform {
+    required_version = "0.13.5"
+    required_providers {
+        aws =- ">=3.23.0"
+    }
+}
---
+++ tf/var.tf
@@ -0,0 +1,4 @@
+variable "your_ip_addr" {
+    type = string
+    default = "218.238.107.0/24"       ## 네이버에서 "내아이피" 로 검색한 후, 결과값을 CIDR 형태로 입력.
+}
